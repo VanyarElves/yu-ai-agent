fd5f12763d7542a002e7d16d8e7d8e2cb98af6ed
Vanyar <gzt44732@163.com>
2026-06-05 17:44:45 +0800

    Initial commit

---
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/OllamaAiInvoke.java
@@ -0,0 +1,26 @@
+package com.yupi.yuaiagent.demo.invoke;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring AI 框架调用阿里AI大模型
+ */
+@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("你好，我是豌豆射手"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
---
+++ src/test/java/com/yupi/yuaiagent/app/LoveAppTest.java
@@ -0,0 +1,12 @@
+package com.yupi.yuaiagent.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoveAppTest {
+
+    @Test
+    void doChat() {
+    }
+}
