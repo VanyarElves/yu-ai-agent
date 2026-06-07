24a19f454ad0a040ff547032aaa3a79f2a53fa42
Vanyar <gzt44732@163.com>
2026-06-07 20:10:53 +0800

    更新基于PGVector实现向量存储

--- src/main/java/com/yupi/yuaiagent/demo/invoke/LangChainAiInvoke.java
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/LangChainAiInvoke.java
@@ -1,4 +1,15 @@
 package com.yupi.yuaiagent.demo.invoke;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatModel;
+
 public class LangChainAiInvoke {
+    public static void main(String[] args) {
+        ChatModel qwenChatModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-plus")
+                .build();
+        String answer=qwenChatModel.chat("我是豌豆射手，我想进化得更厉害！");
+        System.out.println(answer);
+    }
 }
--- src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
@@ -1,4 +1,5 @@
 package com.yupi.yuaiagent.demo.invoke;
 
 public interface TestApiKey {
+    String API_KEY = "sk-bc174cd08ee1486a98ba6b2753f10fd6";
 }
